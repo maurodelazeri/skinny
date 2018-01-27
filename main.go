1ee6324e53732b3c6223aff47b22095f53079697
Mauro Delazeri <maurodelazeri@gmail.com>
2018-01-27 04:18:33 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package skinny
 
 import (
 	"fmt"
@@ -149,27 +149,4 @@ func (m *Metric) GetRange(from uint64, to uint64) []*Point { // Slice of points?
 		current = current.next
 	}
 	return result
-}
-
-func main() {
-	testmetric := Metric{capacity: 131487, indexinterval: 1440} // 3 months of per minute, indexed on days
-	testmetric.Init()
-
-	test := map[string]interface{}{
-		"test": "mauro",
-	}
-
-	testmetric.Insert(1416585010, test, true)
-	testmetric.Insert(1416585011, test, true)
-	testmetric.Insert(1416585012, test, true)
-	testmetric.Insert(1416585012, test, true)
-	testmetric.Insert(1416585012, test, true)
-	testmetric.Insert(1416585013, test, true)
-	testmetric.Insert(1416585014, test, true)
-	testmetric.Insert(1416585015, test, true)
-	testmetric.Insert(1416585015, test, true)
-
-	for _, pnt := range testmetric.GetRange(1416585013, 1416585015) {
-		fmt.Printf("Found: %+v\n", pnt)
-	}
 }
