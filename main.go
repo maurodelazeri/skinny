d5eff1e06c0755b968080da9deb31c0c3fff1f53
Mauro Delazeri <maurodelazeri@gmail.com>
2018-02-14 22:01:44 -0500

    exporting

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package skinny
 
 // Single point
 type Point struct {
-	timestamp uint64
-	value     map[string]interface{}
-	next      *Point
+	Timestamp uint64
+	Value     map[string]interface{}
+	Next      *Point
 }
 
 // Core linked list, main storage array.
@@ -29,14 +29,14 @@ func (m *Metric) Init() {
 
 // Add a pointer to the search index if needed, else do nothing and return.
 func (m *Metric) indexPtr(p *Point) {
-	bucket := uint64(int(p.timestamp / m.Indexinterval))
+	bucket := uint64(int(p.Timestamp / m.Indexinterval))
 	if _, ok := m.index[bucket]; ok {
 		return // bail, point already indexed.
 	}
-	if p.timestamp < m.indexoldest { // We need to index a point further in the past than our index covers.
+	if p.Timestamp < m.indexoldest { // We need to index a point further in the past than our index covers.
 		m.indexoldest = bucket
 	}
-	if p.timestamp < m.indexnewest {
+	if p.Timestamp < m.indexnewest {
 		return // bail, don't need to index a point in the past.
 	}
 	if len(m.index) == 0 { //point is first in index.
@@ -49,21 +49,21 @@ func (m *Metric) indexPtr(p *Point) {
 // left trim for the metric stack.
 func (m *Metric) Ltrim(t int) {
 	for i := 0; i <= t; i++ {
-		nextBucket := uint64(int(m.oldest.next.timestamp / m.Indexinterval))
-		if nextBucket == m.indexoldest { // If the truncate does not push us into the next bucket.
-			m.index[nextBucket] = m.oldest.next // shift right
+		NextBucket := uint64(int(m.oldest.Next.Timestamp / m.Indexinterval))
+		if NextBucket == m.indexoldest { // If the truncate does not push us into the Next bucket.
+			m.index[NextBucket] = m.oldest.Next // shift right
 		} else { // if it does.
 			delete(m.index, m.indexoldest) // drop the empty bucket.
-			m.indexoldest = nextBucket     // reset the indexoldest
+			m.indexoldest = NextBucket     // reset the indexoldest
 		}
-		m.oldest = m.oldest.next //reset metric oldest.
+		m.oldest = m.oldest.Next //reset metric oldest.
 		m.length--
 	}
 }
 
-// Given time(t) and value(v) create a new point and add it to the metric list.
-func (m *Metric) Insert(timestamp uint64, value map[string]interface{}, overwriting bool) {
-	var p = &Point{timestamp, value, nil}
+// Given time(t) and Value(v) create a new point and add it to the metric list.
+func (m *Metric) Insert(Timestamp uint64, Value map[string]interface{}, overwriting bool) {
+	var p = &Point{Timestamp, Value, nil}
 	// check for overflow, deindex, and truncate oldest.
 	if m.length >= m.Capacity {
 		m.Ltrim(int(m.length - m.Capacity))
@@ -77,24 +77,24 @@ func (m *Metric) Insert(timestamp uint64, value map[string]interface{}, overwrit
 		return
 	}
 	if overwriting {
-		m.newest.next = p // set the newest element next reference to the new point.
+		m.newest.Next = p // set the newest element Next reference to the new point.
 		m.newest = p      //set newest node to new point.
 		m.length++
 		m.indexPtr(p)
 		return
 	} else {
 		// if our new point is newest in series.
-		if m.newest.timestamp < p.timestamp {
-			m.newest.next = p // set the newest element next reference to the new point.
+		if m.newest.Timestamp < p.Timestamp {
+			m.newest.Next = p // set the newest element Next reference to the new point.
 			m.newest = p      //set newest node to new point.
 			m.length++
 			m.indexPtr(p)
 			return
 		}
 		// do an evil past tense insert.
-		if m.newest.timestamp >= p.timestamp {
-			if m.newest.timestamp == p.timestamp {
-				m.newest.value = value // Slight optimization for overwriting the newest point.
+		if m.newest.Timestamp >= p.Timestamp {
+			if m.newest.Timestamp == p.Timestamp {
+				m.newest.Value = Value // Slight optimization for overwriting the newest point.
 				return
 			}
 			// stub
@@ -105,22 +105,26 @@ func (m *Metric) Insert(timestamp uint64, value map[string]interface{}, overwrit
 	// If we get here signal an error.
 }
 
-// Given time(t) return a pointer for that point or the next in list.
+// Given time(t) return a pointer for that point or the Next in list.
 func (m *Metric) Search(t uint64) *Point {
 	bucket := uint64(int(t / m.Indexinterval))
 	startpoint := m.index[bucket]
+	// prevent acessing nil point
+	if startpoint == nil {
+		return &Point{0, nil, nil}
+	}
 	current := startpoint
 	if t == 0 {
 		return m.oldest
 	}
 	for {
-		if current.timestamp >= t {
+		if current.Timestamp >= t {
 			return current
 		} else {
-			if current.next == nil {
+			if current.Next == nil {
 				return &Point{0, nil, nil} //return empty point
 			}
-			current = current.next
+			current = current.Next
 		}
 	}
 }
@@ -135,14 +139,14 @@ func (m *Metric) GetRange(from uint64, to uint64) []*Point { // Slice of points?
 
 	current := startpoint
 	for {
-		if current.timestamp > to {
+		if current.Timestamp > to {
 			break
 		}
 		result = append(result, current)
-		if current.next == nil {
+		if current.Next == nil {
 			break
 		}
-		current = current.next
+		current = current.Next
 	}
 	return result
 }
